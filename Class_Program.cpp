b3d684f4a0e49137e7495be27fcf3535a8663288
Raqeeb_Raees <raqeebraees23@gmail.com>
2024-09-02 10:45:15 -0700

    Working in Remove function

--- Class_Program.cpp
+++ Class_Program.cpp
@@ -75,25 +75,55 @@ class ArrayList
         }
     }
 
-        void Find_position(int value)
+        int Find_position(int value)
         {
             if(length==0)
             {
                 cout<<"\nThe list is empty!!!"<<endl;
-                return;
+                return 0;
             }
 
             Ptr_to_Start();
-            for( int i=0; i<length-1; i++)
+            for( int i=0; i<length; i++)
             {
                 if(*(Curr_ptr+i)==value)
                 {
-                        
+                        return i+1;
                 }
             }
+
+            return 0;
             
         }
 
+        void Remove_by_value(int value)
+        {
+            if(length==0)
+            {
+                cout<<"\nthe list is empty\n";
+                return;
+            }
+            
+
+            int Position_of_value=Find_position(value);
+
+            while(1)
+            {
+                Ptr_to_Start();
+                Curr_ptr+=Position_of_value-1;
+                for(int i=length; i<capacity;i++)
+                {
+                    *(Curr_ptr)=*(Curr_ptr+1);
+                }
+                if(Position_of_value=(Find_position(value))!=0)
+                     --length;
+                else 
+                break;
+
+            }
+
+            
+        }
 
 
 };
@@ -102,17 +132,51 @@ class ArrayList
 
     ArrayList Obj1(5);
     Obj1.Random_array_input();
+    while(1)
+{
     cout<<"\nWhat you want to with the list?"<<endl;
-    cout<<"1)Insert the new value";
+    cout<<"1)Insert the new value\n2)Find position of value\n3)Remove the value\n";
     cout<<"\nUser input:";
     int user_input;
+    cin>>user_input;
+    if(user_input==1)
+    {
+    cout<<"Enter value to insert:";
     cin>>user_input;
     int position;
     cout<<"Position:";
     cin>>position;
     Obj1.insert(user_input, position);
     Obj1.Display_The_Array();
-
+    }
+    else if(user_input==2)
+    {
+        cout<<"\nEnter value to find its position:";
+        cin>>user_input;
+        
+        if(Obj1.Find_position(user_input) !=0)
+        {
+            cout<<"Position of the value"<<user_input<<" is "<< Obj1.Find_position(user_input)<<endl;
+            Obj1.Display_The_Array();
+        }
+        else if(Obj1.Find_position(user_input)==0)
+        {
+            cout<<"\nList is empty or the value is not in the list\n"<<endl;
+        }
+    }
+    else if(user_input==3)
+    {
+        cout<<"\nEnter the value to remove it from the Array list:";
+        cin>>user_input;
+        Obj1.Remove_by_value(user_input);
+        Obj1.Display_The_Array();
+    }
+    else if(user_input==0)
+    {
+        cout<<"THank you for using this program";
+        break;
+    }
+}
 
     return 0;
  }
