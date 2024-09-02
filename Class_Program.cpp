cfd93440e376881b777dba8cbb66dec79e388086
Raqeeb_Raees <raqeebraees23@gmail.com>
2024-09-02 09:30:28 -0700

    Data Structure coe

---
+++ Class_Program.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+
+class ArrayList
+{
+    private:
+        int *Array_ptr;
+        int capacity;
+        int length;
+        int *Curr_ptr;
+
+    public:
+
+        ArrayList(int size)
+        {
+            Array_ptr= new int[size];
+            capacity=size;
+            length=0;
+            Curr_ptr=NULL;
+        }
+
+`   
+        void insert( int )
+
+};
+ int main(void)
+ {
+
+
+
+
+    return 0;
+ }
